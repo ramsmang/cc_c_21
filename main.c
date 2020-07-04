47bae6ce67311213ce0e36aa0d4ea7b6c841a2cd
Sheamang Ramsford <ramsfordsheamang@gmail.com>
2020-07-04 09:00:49 +0000

    nothing

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "game.h"
+
+int main()
+
+{
+    t_game * g = game_init();
+
+    game_loop(g);
+    return 0;
+int p_hand;
+p_hand = ;    
+print_player_coins(g->player); // Afficher le nombre de jetons du joueur
+broker_new_hand(g); // Le croupier pioche
+player_new_hand(g); // Le joueur pioce
+print_player_hand(g->player); // Afficher la main du joueur
+broker_place_bet(g); // Le croupier mise
+player_place_bet(g); // Le joueur mise
+sum_player_hand(g->player); /* Le joueur choisi la valeur de ses Valets
+ et cette function "return" le score du joueur */
+print_turn_results(g, p_hand); /* Afficher le score du croupier et du joueur
+  (le score du joueur doit être donné par la
+ variable p_hand) */
+ 
+}
+
+void game_loop(t_game * g)
+{
+
+}
