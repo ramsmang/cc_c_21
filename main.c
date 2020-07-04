c85539b596c2c967a9f6d80bba04147a6bbc0f81
Sheamang Ramsford <ramsfordsheamang@gmail.com>
2020-07-04 12:47:39 +0000

    suite

--- main.c
+++ main.c
@@ -2,26 +2,49 @@
 
 #include "game.h"
 
-int main()
+int p_hand = 0;
+int b_hand = 0;
 
+int main()
 {
     t_game * g = game_init();
 
     game_loop(g);
     return 0;
-
 }
 
 void game_loop(t_game * g)
 {
-int p_hand;
-p_hand = 0;
 print_player_coins(g->player);
 broker_new_hand(g);
 player_new_hand(g);
 print_player_hand(g->player);
 broker_place_bet(g);
-player_place_bet(g);
+player_place_bet(g); 
 sum_player_hand(g->player);
 print_turn_results(g, p_hand);
+
+
+if (p_hand > 21)
+{
+  player_breakthrough(g);
+  player_lost(g);
+}
+
+if (p_hand == 21)
+{
+  player_jackpot(g);
+  player_win(g);
+}
+
+if (p_hand > b_hand)
+{
+    player_win(g);
+}
+
+if (p_hand < b_hand)
+{
+    player_lost(g);
+}
+player_ask_cards(g);
 }
